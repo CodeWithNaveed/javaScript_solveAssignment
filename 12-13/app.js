22abcc854038a8c462fdb1f07647e766ef5df928
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-04-04 14:56:45 +0500

    add new assignment file

---
+++ 12-13/app.js
@@ -0,0 +1,152 @@
+// let a = +prompt("Enter a number");
+
+// if (a > 0 ) {
+//     console.log(`${a} is a Positive number`);
+// }
+
+// else if (a < 0 ) {a
+//     console.log(`${a} is a Negative number`);
+// }
+
+// else if (a == 0) {
+//     console.log(`${a} is Zero`);
+// }
+
+// else {
+//     console.log(`${a} is not a number`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let vowels = prompt("Enter a letter");
+
+// if (vowels == "a" || vowels == "e" || vowels == "i" || vowels == "o" || vowels == "u") {
+//     console.log(`"${vowels}" is a vowel`);
+// }
+// else {
+//     console.log(`"${vowels}" is not a vowel`);
+// }
+
+
+
+
+
+
+
+
+
+// let password = +prompt("Enter your password") && prompt("Enter your password");
+// let confirmPassword = +prompt("Confirm your password");
+
+// if(password == confirmPassword) {
+//     alert("Correct! The password you entered matches the original password");
+// }
+
+// else (password !== confirmPassword) {
+//     alert("password does not match");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = +prompt("Enter an integer");
+// let num2 = +prompt("Enter another integer");
+// function larger(num1, num2) {
+//     if(num1 > num2) {
+//         alert(`${num1} is larger than ${num2}`);
+//     }
+//     else if(num2 > num1) {
+//         alert(`${num2} is larger than ${num1}`);
+//     }
+//     else {
+//         console.log(`${num1} and ${num2} are equal`);
+//     }
+// }
+
+// larger(num1, num2);
+
+
+
+
+
+
+
+
+
+// let number = +prompt("Enter a number");
+
+// if (number > 0) {
+//     alert(number + " is a positive number");
+// }
+
+// else if (number < 0) {
+//     alert(number + " is a negative number");
+// }
+// else {
+//     alert(number + " is zero");
+// }
+
+
+
+
+
+
+
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//    document.write (greeting = "Good day");
+// } else {
+//     document.write(greeting = "Good evening");
+// }
+
+
+
+
+
+
+
+
+
+
+// var time = +prompt("Enter time in 24 hour format:");
+
+// if (time < 1200) {
+//     console.log("Good morning");
+// }
+// else if (time >= 1200 && time < 1800) {
+//     console.log("Good afternoon");
+// }
+// else if (time >= 1800 && time < 2400) {
+//     console.log("Good evening");
+// }
+// else {
+//     console.log("Good night");
+// }
+         
+
+
+
+
+
+
+
+
