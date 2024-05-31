824a6b2c5aad724f61fddf3054964d468e39fecb
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-05-31 08:33:33 +0500

    Assignment 43-48

---
+++ 43-48/app.js
@@ -0,0 +1,65 @@
+// alert ("welcome in our mobile shope")
+// function thanku(){
+//     alert("Thanks for purchasing phone from us")
+// }
+
+
+
+// const studentsName = ["sakib","Jahangir","Hossain","wahid"]
+// const table = document.createElement("table");
+// document.body.appendChild(table);
+// for (let i = 0; i < 4; i++) {
+//     const row = table.insertRow();
+//     const nameCell = row.insertCell();
+//     const buttonCell = row.insertCell();
+//     nameCell.innerHTML = `${i}: ${studentsName[i]}`;
+//     buttonCell.innerHTML = "<button>Delete</button>";
+
+//     buttonCell.children[0].addEventListener("click",function(){
+//         row.remove();
+//     })
+// }
+
+
+
+
+
+// const img = document.createElement("img");
+// img.src = "https://tse1.mm.bing.net/th?id=OIP.cfN0JdZIyf8wYlluqNb87wHaEK&pid=Api&P=0&h=180";
+// document.body.appendChild(img);
+
+// img.addEventListener("mouseover", function() {
+//     img.style.cursor = "pointer";
+//     img.src = "https://tse1.mm.bing.net/th?id=OIP.iXFvb8jCMYUWRqgjhM6_CQHaDK&pid=Api&P=0&h=180";
+// });
+
+// img.addEventListener("mouseout", function() {
+//     img.src = "https://tse1.mm.bing.net/th?id=OIP.cfN0JdZIyf8wYlluqNb87wHaEK&pid=Api&P=0&h=180";
+// });
+
+
+
+
+// let counter = 0;
+
+// const increaseButton = document.createElement("button");
+// increaseButton.innerHTML = "Increase";
+// document.body.appendChild(increaseButton);
+
+// const decreaseButton = document.createElement("button");
+// decreaseButton.innerHTML = "Decrease";
+// document.body.appendChild(decreaseButton);
+
+// const counterDisplay = document.createElement("div");
+// counterDisplay.innerHTML = counter;
+// document.body.appendChild(counterDisplay);
+
+// increaseButton.addEventListener("click", function() {
+//     counter++;
+//     counterDisplay.innerHTML = counter;
+// })
+
+// decreaseButton.addEventListener("click", function() {
+//     counter--;
+//     counterDisplay.innerHTML = counter;
+// })
